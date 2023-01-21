0730eb2a2f947c204d194cce1107faead6430f70
msfir <ms.firdaus777@gmail.com>
2023-01-21 18:00:52 +0700

    feat: menyimpan solusi ke dalam file

--- src/main.c
+++ src/main.c
@@ -2,10 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <errno.h>
 #include "solver.h"
 
 #define MAX_INPUT_BUFFER 4
 #define CARDS_LENGTH 13
+#define MAX_FILE_NAME_LENGTH 100
+
+static const const char* CARDS[CARDS_LENGTH] = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
 
 bool parse_input(const char* input, int* output) {
   int val;
@@ -37,11 +41,24 @@ bool parse_input(const char* input, int* output) {
   return false;
 }
 
-static const const char* CARDS[CARDS_LENGTH] = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+void save_solutions(const char* file_name, const char cards[4][4], const char solutions[][22], const int num_of_solutions) {
+  FILE* file;
+  file = fopen(file_name, "w");
+  if (errno) {
+    perror("Gagal menyimpan solusi");
+    exit(1);
+  }
+  fprintf(file, "Kartu: %s %s %s %s\n", cards[0], cards[1], cards[2], cards[3]);
+  fprintf(file, "Solusi:");
+  for (int i = 0; i < num_of_solutions; i++) {
+    fprintf(file, "\n%d. %s", i + 1, solutions[i]);
+  }
+  fclose(file);
+}
 
 void generate_cards(char cards[4][4], int cards_int[4]) {
   time_t t;
-  srand((unsigned) time(&t));
+  srand(time(&t));
   for (int i = 0; i < 4; i++) {
     int idx = rand() % CARDS_LENGTH;
     sprintf(cards[i], "%s", CARDS[idx]);
@@ -64,6 +81,9 @@ int main_menu() {
     printf("Masukan: ");
     scanf("%d", &choice);
   } while (choice < 1 || choice > 3);
+  // membersihkan input buffer
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF) { }
   return choice;
 }
 
@@ -89,9 +109,28 @@ int main() {
   }
   printf("Kartu: %s %s %s %s\n", cards[0], cards[1], cards[2], cards[3]);
   int n = make24(cards_int[0], cards_int[1], cards_int[2], cards_int[3], solutions);
-  printf("Ditemukan %d solusi:\n", n);
-  for (int i = 0; i < n; i++) {
-    printf("%2d. %s\n", i + 1, solutions[i]);
+  if (n > 0) {
+    printf("Ditemukan %d solusi:\n", n);
+    for (int i = 0; i < n; i++) {
+      printf("%d. %s\n", i + 1, solutions[i]);
+    }
+    char ans[MAX_INPUT_BUFFER];
+    do {
+      printf("Apakah Anda ingin menyimpan solusi? (y/[n]) ");
+      fgets(ans, MAX_INPUT_BUFFER, stdin);
+    } while (ans[0] != 'y' && ans[0] != 'n' && ans[0] != '\n');
+    if (ans[0] == 'y') {
+      char file_name[MAX_FILE_NAME_LENGTH];
+      printf("Masukkan nama file: ");
+      fgets(file_name, MAX_FILE_NAME_LENGTH, stdin);
+      int len = str_length(file_name);
+      if (len > 0 && file_name[len - 1] == '\n') {
+        file_name[--len] = '\0';
+      }
+      save_solutions(file_name, cards, solutions, n);
+    }
+  } else {
+    puts("Tidak ditemukan solusi");
   }
   return 0;
 }
